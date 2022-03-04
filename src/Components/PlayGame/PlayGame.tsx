be0171f2921bd3c097e1d696316bfcc5807d4c28
ClaytonDewey <clay@dryadmedia.com>
2022-03-04 12:29:32 -0600

    Start game play layout

--- src/Components/PlayGame/PlayGame.tsx
+++ src/Components/PlayGame/PlayGame.tsx
@@ -8,9 +8,29 @@ export const PlayGame = () => {
         nav(-2);
     };
 
+    const nextTurn = () => {
+        alert("Next move...")
+    }
+
     return (
         <div className="container container-play">
             <h1 className="text-center my-2">Play Game</h1>
+            <div className="container-points">
+                <div className="form-control">
+                    <input id="points-hand" type="number" required />
+                    <label><span>Hand Points</span></label>
+                </div>
+
+                <div className="form-control">
+                    <input id="points-crib" type="number" required />
+                    <label><span>Crib Points</span></label>
+                </div>
+            </div>
+
+            <button className="btn btn-info mt-2" onClick={nextTurn}>
+                Next Turn
+            </button>
+
             <button className="btn btn-success mt-2" onClick={endGame}>
                 Done
             </button>
