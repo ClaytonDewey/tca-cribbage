8d7b401bf345ee3bef716f6b9c093af3a691a4e8
ClaytonDewey <clay@dryadmedia.com>
2022-03-04 09:22:41 -0600

    Start work on Setup Game form

--- src/Components/SetupGame/SetupGame.js
+++ src/Components/SetupGame/SetupGame.js
@@ -3,11 +3,47 @@ import { useNavigate } from "react-router-dom";
 
 export const SetupGame = () => {
     const nav = useNavigate();
+
     return (
         <div className="container container-setup">
             <h1 className="text-center my-2">Setup Game</h1>
+            <h2 className="text-center my-2">Add New Oppenent</h2>
+            <div className="form-control">
+                <input id="new-opp" type="text" required />
+                <label>
+                    <span>Oppenent Name</span>
+                </label>
+                <button>Add</button>
+            </div>
 
-            <button className="btn btn-success mt-2" onClick={() => nav("/play")}>
+            <h2 className="text-center mt-4 mb-2">Select Oppenent</h2>
+            <ul className="form-check-control">
+                <li>
+                    <label>
+                        <input name="opponent" type="radio" />
+                        Dad
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input name="opponent" type="radio" />
+                        Michael
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input name="opponent" type="radio" />
+                        William
+                    </label>
+                </li>
+                <li>
+                    <label>
+                        <input name="opponent" type="radio" />
+                        Mary
+                    </label>
+                </li>
+            </ul>
+            <button className="btn btn-success mt-4" onClick={() => nav("/play")}>
                 Start Game
             </button>
         </div>
