724f943727a29e8955f24c3d2f3c1288bbf96ff0
ClaytonDewey <clay@dryadmedia.com>
2022-03-09 17:18:49 -0600

    Reformatting skunks/skunked functions

--- src/Components/Stats/Stats.tsx
+++ src/Components/Stats/Stats.tsx
@@ -1,43 +1,49 @@
 import { useNavigate } from "react-router-dom";
+const calcPercentage = (results, who: string): number => {
+    const percentage: number = results.filter(x => x.winner === who).length / results.length;
+
+    return percentage < 1 ? +percentage.toFixed(2) * 100 : percentage * 100;
+};
+
+const calculateTotalWins = (results, who: string): number => (results.filter(x => x.winner === who).length);
+
+const calcSkunks = (results, what: string): number => {
+    let count = 0;
+    results.map(x => x[what] ? count++ : count)
+    return count;
+}
+
+// const calcSkunks = (results, who: string): number => {
+//     let skunks = 0;
+//     results.map(x => x.skunk ? skunks++ : skunks)
+//     return skunks;
+// }
+
+// const calcDblSkunks = (results, who: string): number => {
+//     let dblSkunks = 0;
+//     results.map(x => x.dblSkunk ? dblSkunks++ : dblSkunks)
+//     return dblSkunks;
+// }
+
+// const calcSkunked = (results, who: string): number => {
+//     let skunked = 0;
+//     results.map(x => x.skunked ? skunked++ : skunked);
+//     return skunked;
+// }
+
+// const calcDblSkunked = (results): number => {
+//     let dblSkunked = 0;
+//     results.map(x => x.dblSkunked ? dblSkunked++ : dblSkunked);
+//     return dblSkunked;
+// }
+
+const highestHand = (results): number => {
+    const highHand = results.map(x => x.highHand);
+    return Math.max(...highHand);
+}
 
 export const Stats = ({ gameResults }) => {
     const nav = useNavigate();
-
-    const calcPercentage = (results, who: string): number => {
-        const percentage: number = results.filter(x => x.winner === who).length / results.length;
-        
-        return percentage < 1 ? +percentage.toFixed(2) * 100 : percentage * 100;
-    };
-
-    const calculateTotalWins = (results, who: string): number => (results.filter(x => x.winner === who).length);
-
-    const calcSkunks = (results, who: string):number => {
-        let skunks = 0;
-        results.map(x => x.skunk ? skunks++ : skunks)
-        return skunks;
-    }
-
-    const calcDblSkunks = (results, who: string):number => {
-        let dblSkunks = 0;
-        results.map(x => x.dblSkunk ? dblSkunks++ : dblSkunks)
-        return dblSkunks;
-    }
-
-    const calcSkunked = (results, who: string):number => {
-        let skunked = 0;
-        results.map(x => x.skunked ? skunked++ : skunked);
-        return skunked;
-    }
-
-    const calcDblSkunked = (results):number => {
-        let dblSkunked = 0;
-        results.map(x => x.dblSkunked ? dblSkunked++ : dblSkunked);
-        return dblSkunked;
-    }
-    const highestHand = (results):number => {
-        const highHand = results.map(x => x.highHand);
-        return Math.max(...highHand);
-    }
 
     return (
         <div className="container">
@@ -63,19 +69,19 @@ export const Stats = ({ gameResults }) => {
                     Losses
                 </div>
                 <div className="stat">
-                    <span>{calcSkunks(gameResults, "Me")}</span>
+                    <span>{calcSkunks(gameResults, "skunks")}</span>
                     Skunks
                 </div>
                 <div className="stat">
-                    <span>{calcDblSkunks(gameResults, "Me")}</span>
+                    <span>{calcSkunks(gameResults, "dblSkunks")}</span>
                     Dbl Skunks
                 </div>
                 <div className="stat">
-                    <span>{calcSkunked(gameResults, "Me")}</span>
+                    <span>{calcSkunks(gameResults, "skunked")}</span>
                     Skunked
                 </div>
                 <div className="stat">
-                    <span>{calcDblSkunked(gameResults)}</span>
+                    <span>{calcSkunks(gameResults, "dblSkunked")}</span>
                     Dbl Skunked
                 </div>
                 <div className="stat">
