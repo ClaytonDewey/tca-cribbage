c8b83124c25eb281642ecdaaa8cfdfee00006545
ClaytonDewey <clay@dryadmedia.com>
2022-03-10 12:55:39 -0600

    Added an optional close button.

--- src/Components/Messages/Messages.tsx
+++ src/Components/Messages/Messages.tsx
@@ -1,7 +1,8 @@
-export const Message = (message: {type: string, msg: string}) => {
+export const Message = (message: {type: string, msg: string, toggle?: boolean }) => {
     return (
         <div className={message.type}>
             <p>{message.msg}</p>
+            { message.toggle && (<i className="close fa fa-times"></i>) }
         </div>
     )
 }
