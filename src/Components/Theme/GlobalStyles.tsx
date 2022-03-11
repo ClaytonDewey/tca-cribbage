2776507643771f91e3a7c029339f0eebaa3ef918
ClaytonDewey <clay@dryadmedia.com>
2022-03-11 08:23:34 -0600

    Added mode toggle button styles

--- src/Components/Theme/GlobalStyles.tsx
+++ src/Components/Theme/GlobalStyles.tsx
@@ -5,4 +5,9 @@ body {
     color: ${({ theme }) => theme.text};
     transition: background-color 0.5s ease-in, color 0.5s ease-in;
 }
+
+.btn-toggle {
+    background-color: ${({ theme }) => theme.buttonBg };
+    color: ${({ theme }) => theme.buttonTxt};
+}
 `
