7d533180c161892dd7eb7c043c55a25b6c19e7b0
ClaytonDewey <clay@dryadmedia.com>
2022-03-11 07:24:23 -0600

    Created Themes.tsx

---
+++ src/Components/Themes.tsx
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: "#e1e1e1"
+    , text: "#3e3e3e"
+    , toggleBorder: "#e1e1e1"
+    , background: "#3e3e3e"
+}
+export const darkTheme = {
+    body: "#3e3e3e"
+    , text: "#e1e1e1"
+    , toggleBorder: "#3e3e3e"
+    , background: "#3e3e3e"
+}
