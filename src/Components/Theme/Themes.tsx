6eac4c4fe17f4e4413589eeaa51e0916c479a4c3
ClaytonDewey <clay@dryadmedia.com>
2022-03-11 08:33:50 -0600

    Changed button border color

--- src/Components/Theme/Themes.tsx
+++ src/Components/Theme/Themes.tsx
@@ -1,7 +1,7 @@
 export const lightTheme = {
     body: "#e1e1e1"
     , text: "#3e3e3e"
-    , toggleBorder: "#e1e1e1"
+    , toggleBorder: "#3e3e3e"
     , background: "#3e3e3e"
     , buttonBg: "#0dcaf0"
     , buttonTxt: "#3e3e3e"
@@ -9,7 +9,7 @@ export const lightTheme = {
 export const darkTheme = {
     body: "#3e3e3e"
     , text: "#e1e1e1"
-    , toggleBorder: "#3e3e3e"
+    , toggleBorder: "#e1e1e1"
     , background: "#3e3e3e"
     , buttonBg: "#5cb85c"
     , buttonTxt: "#e1e1e1"
