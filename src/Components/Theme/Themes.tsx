096890841860ab377a2b82579ba64062738a78f5
ClaytonDewey <clay@dryadmedia.com>
2022-03-11 08:23:48 -0600

    Updated Themes.tsx

--- src/Components/Theme/Themes.tsx
+++ src/Components/Theme/Themes.tsx
@@ -3,10 +3,14 @@ export const lightTheme = {
     , text: "#3e3e3e"
     , toggleBorder: "#e1e1e1"
     , background: "#3e3e3e"
+    , buttonBg: "#0dcaf0"
+    , buttonTxt: "#3e3e3e"
 }
 export const darkTheme = {
     body: "#3e3e3e"
     , text: "#e1e1e1"
     , toggleBorder: "#3e3e3e"
     , background: "#3e3e3e"
+    , buttonBg: "#5cb85c"
+    , buttonTxt: "#e1e1e1"
 }
