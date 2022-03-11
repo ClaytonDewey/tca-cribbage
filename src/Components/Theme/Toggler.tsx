4b585925f299454c1caf1b97e0d1f3f598e878f3
ClaytonDewey <clay@dryadmedia.com>
2022-03-11 13:19:23 -0600

    Working on custom picker button
    Currently doesn't remember checked state...

--- src/Components/Theme/Toggler.tsx
+++ src/Components/Theme/Toggler.tsx
@@ -1,26 +1,39 @@
 import React from "react";
 import { func, string } from "prop-types";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Button = styled.button`
-  background: ${({ theme }) => theme.buttonBg};
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
-  color: ${({ theme }) => theme.buttonTxt};
-  border-radius: 30px;
-  cursor: pointer;
-  font-size:1.4rem;
-  padding: 1rem 2rem;
-  text-transform: capitalize;
-  font-weight: 700;
-`;
+// const Button = styled.button`
+//   background: ${({ theme }) => theme.buttonBg};
+//   border: 2px solid ${({ theme }) => theme.toggleBorder};
+//   color: ${({ theme }) => theme.buttonTxt};
+//   border-radius: 30px;
+//   cursor: pointer;
+//   font-size:1.4rem;
+//   padding: 1rem 2rem;
+//   text-transform: capitalize;
+//   font-weight: 700;
+// `;
 
-const Toggle = ({theme, toggleTheme }) => {
+// const Toggle = ({theme, toggleTheme }) => {
+//     return (
+//         <Button className="btn-toggle" onClick={toggleTheme} >
+//             Switch Mode
+//         </Button>
+//     );
+// };
+
+const Toggle = ({ theme, toggleTheme }) => {
     return (
-        <Button className="btn-toggle" onClick={toggleTheme} >
-            Switch Mode
-        </Button>
-    );
-};
+        <div className="toggle-container">
+            <input type="checkbox" id="mode" className="toggle" onChange={toggleTheme} />
+            <label htmlFor="mode" className="label">
+                <div className="ball"></div>
+            </label>
+            <span>{theme} Mode</span>
+        </div>
+    )
+}
+
 Toggle.protoTypes = {
     theme: string.isRequired
     , toggleTheme: func.isRequired
