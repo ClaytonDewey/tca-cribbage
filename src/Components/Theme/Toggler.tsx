9b79e2a48951eb482ba3e36c764342a7a1db4003
ClaytonDewey <clay@dryadmedia.com>
2022-03-13 13:53:50 -0500

    Removed unused code

--- src/Components/Theme/Toggler.tsx
+++ src/Components/Theme/Toggler.tsx
@@ -1,26 +1,4 @@
-import React from "react";
 import { func, string } from "prop-types";
-// import styled from "styled-components";
-
-// const Button = styled.button`
-//   background: ${({ theme }) => theme.buttonBg};
-//   border: 2px solid ${({ theme }) => theme.toggleBorder};
-//   color: ${({ theme }) => theme.buttonTxt};
-//   border-radius: 30px;
-//   cursor: pointer;
-//   font-size:1.4rem;
-//   padding: 1rem 2rem;
-//   text-transform: capitalize;
-//   font-weight: 700;
-// `;
-
-// const Toggle = ({theme, toggleTheme }) => {
-//     return (
-//         <Button className="btn-toggle" onClick={toggleTheme} >
-//             Switch Mode
-//         </Button>
-//     );
-// };
 
 const Toggle = ({ theme, toggleTheme }) => {
     return (
