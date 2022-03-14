0a58b6fe509891aac926b3acd371d52ceb909999
ClaytonDewey <clay@dryadmedia.com>
2022-03-14 10:02:39 -0500

    Add conditional displays for number of players

--- src/Components/SetupGame/SetupGame.tsx
+++ src/Components/SetupGame/SetupGame.tsx
@@ -11,6 +11,8 @@ export const SetupGame: React.FC<SetupGameProps> = ({ getUniquePlayers }) => {
     const [opponents, setOpponents] = useState([...getUniquePlayers].sort().map(x => ({ name: x, checked: false })));
     const [newOpponent, setNewOpponent] = useState("");
     const [message, setMessage] = useState({ type: "", msg: "" });
+    const [players, setPlayers] = useState(0);
+    const [click, setClick] = useState(false);
 
     const hideMsg = () => {
         setTimeout(() => {
@@ -66,8 +68,19 @@ export const SetupGame: React.FC<SetupGameProps> = ({ getUniquePlayers }) => {
         }
     }
 
+    const selectGameSettings = () => {
+        if (opponents.filter(x => x.checked).length === 1) {
+            setPlayers(2)
+        } else if (opponents.filter(x => x.checked).length === 2) {
+            setPlayers(3)
+        } else {
+            setPlayers(4)
+        }
+        setClick(true);
+    }
+
     const startGame = () => {
-        nav("/play")
+        nav("/play");
     }
 
     return (
@@ -105,25 +118,87 @@ export const SetupGame: React.FC<SetupGameProps> = ({ getUniquePlayers }) => {
                 ))}
             </ul>
 
-            <h2 className="text-center mt-4 mb-2">Who Won the Cut?</h2>
-            <ul className="form-check-control">
-                <li>
-                    <label>
-                        <input type="radio" name="cut" />
-                        Me
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="cut" />
-                        Opponent
-                    </label>
-                </li>
-            </ul>
+            { players === 2 &&
+                <>
+                    <h2 className="text-center mt-4 mb-2">Who Won the Cut?</h2>
+                    <ul className="form-check-control">
+                        <li>
+                            <label>
+                                <input type="radio" name="cut" />
+                                Me
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="cut" />
+                                Opponent
+                            </label>
+                        </li>
+                    </ul>
+                    <button className="btn btn-success mt-4" onClick={startGame}>
+                        Start Game <i className="fa-solid fa-circle-play"></i>
+                    </button>
+                </>
+            }
 
-            <button className="btn btn-success mt-4" onClick={startGame}>
-                Start Game <i className="fa-solid fa-circle-play"></i>
-            </button>
+            { players === 3 &&
+                <>
+                    <h2 className="text-center mt-4 mb-2">Select Turn Order</h2>
+                    <ul className="form-check-control">
+                        <li>
+                            <label>
+                                <input type="radio" name="order" />
+                                1
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="order" />
+                                2
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="order" />
+                                3
+                            </label>
+                        </li>
+                    </ul>
+                    <button className="btn btn-success mt-4" onClick={startGame}>
+                        Start Game <i className="fa-solid fa-circle-play"></i>
+                    </button>
+                </>
+            }
+
+            { players === 4 &&
+                <>
+                    <h2 className="text-center mt-4 mb-2">Who Won the Cut?</h2>
+                    <ul className="form-check-control">
+                        <li>
+                            <label>
+                                <input type="radio" name="cut" />
+                                My Team
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="cut" />
+                                Opposing Team
+                            </label>
+                        </li>
+                    </ul>
+                    <button className="btn btn-success mt-4" onClick={startGame}>
+                        Start Game <i className="fa-solid fa-circle-play"></i>
+                    </button>
+                </>
+            }
+
+            { click === false &&
+                <button className="btn btn-success mt-4" onClick={selectGameSettings}>
+                    Continue <i className="fa-solid fa-circle-play"></i>
+                </button>
+            }
+
         </div>
     );
 };
