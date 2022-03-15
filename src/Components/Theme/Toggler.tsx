7f1df8c46e756b6857f4f20483096aa27d2e8d73
ClaytonDewey <clay@dryadmedia.com>
2022-03-15 12:04:16 -0500

    Playing with pixel perfection...

--- src/Components/Theme/Toggler.tsx
+++ src/Components/Theme/Toggler.tsx
@@ -3,14 +3,15 @@ import { func, string } from "prop-types";
 const Toggle = ({ theme, toggleTheme }) => {
     return (
         <>
+            <i className="fa-solid fa-sun"></i>
             <div className="toggle-container">
                 <input type="checkbox" id="mode" className={`toggle ${theme}`} onChange={toggleTheme} />
                 <label htmlFor="mode" className="label">
                     <div className="ball"></div>
                 </label>
-                <span>{theme} Mode </span>
+                {/* <span>{theme} Mode </span> */}
             </div>
-            {/* <i className={`fa-solid fa-lightbulb  ${theme}`}></i> */}
+            <i className="fa-solid fa-moon"></i>
         </>
     )
 }
