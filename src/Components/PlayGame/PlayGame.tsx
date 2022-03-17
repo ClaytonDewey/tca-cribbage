1da9b32ab12957fe826091c3dff9172644b1c3a2
ClaytonDewey <clay@dryadmedia.com>
2022-03-17 09:37:41 -0500

    Rethinking game play function...

--- src/Components/PlayGame/PlayGame.tsx
+++ src/Components/PlayGame/PlayGame.tsx
@@ -9,50 +9,30 @@ interface PlayerInGame extends Player {
 export const PlayGame = ({ currentGame }) => {
     const nav = useNavigate();
     const { players } = currentGame;
-    const [cribHand, setCribHand] = useState(false);
-    const [activePlayer, setActivePlayer] = useState<PlayerInGame | undefined>(undefined);
-    const [playersInOrder, setPlayersInOrder] = useState<PlayerInGame[]>([]);
+    const [currentPlayer, setCurrentPlayer] = useState<PlayerInGame | undefined>(undefined);
+    const [playerOrder, setPlayerOrder] = useState<PlayerInGame[]>([]);
     
     const orderPlayers = (player: string) => {
-        if (player === "Me") {
-            setCribHand(true);
-                const newPlayer = {
-                name: player
-                , order: playersInOrder.length + 1
-                , currentScore: 0
-            }
-
-            setActivePlayer(newPlayer);
-
-            setPlayersInOrder([
-                ...playersInOrder
-                , newPlayer
-            ]);
-        } else {
-            setCribHand(false);
-            const newPlayer = {
-                name: player
-                , order: playersInOrder.length + 1
-                , currentScore: 0
-            }
-
-            setActivePlayer(newPlayer);
-
-            setPlayersInOrder([
-                ...playersInOrder
-                , newPlayer
-            ]);
+        const nextPlayer = {
+            name: player
+            , order: playerOrder.length + 1
+            , currentScore: 0
         }
+
+        setCurrentPlayer(nextPlayer);
+
+        setPlayerOrder([
+            ...playerOrder
+            , nextPlayer
+        ]);
     }
 
     const nextTurn = (player: PlayerInGame) => {
-        if (playersInOrder.length < currentGame.players.length) {
-            console.log("Active players are NOT yet set...")
-            setActivePlayer(undefined);
+        if(playerOrder.length < players.length) {
+            setCurrentPlayer(undefined);
         } else {
-            console.log("Active players ARE set...")
-            const indexOfActivePlayer = playersInOrder.findIndex(x => x === player);
-            setActivePlayer(indexOfActivePlayer + 1 < playersInOrder.length ? playersInOrder[indexOfActivePlayer] : playersInOrder[0]);
+            const indexOfCurrentPlayer = playerOrder.findIndex(x => x === player);
+            setCurrentPlayer(indexOfCurrentPlayer + 1 < playerOrder.length ? playerOrder[indexOfCurrentPlayer] : playerOrder[0]);
         }
     }
 
@@ -62,10 +42,10 @@ export const PlayGame = ({ currentGame }) => {
 
     return (
         <>
-            <div className={`players-container ${!activePlayer && playersInOrder.length < currentGame.players.length ? "open" : ""}`}>
-                <h2 className="text-center my-2">{`Choose Player ${playersInOrder.length + 1}`}</h2>
+            <div className={`players-container ${!currentPlayer && playerOrder.length < currentGame.players.length ? "open" : ""}`}>
+                <h2 className="text-center my-2">{`Choose Player ${playerOrder.length + 1}`}</h2>
                 {
-                    players.filter(x => playersInOrder.findIndex(y => y.name === x.name) === -1)
+                    players.filter(x => playerOrder.findIndex(y => y.name === x.name) === -1)
                     .map(x => (
                         <button
                             key={x.name} id={x.name}
@@ -79,23 +59,20 @@ export const PlayGame = ({ currentGame }) => {
             </div>
             <div className="container container-play">
                 <h1 className="text-center my-2">Play Game</h1>
+                {/* <h2 className="text-center my-2">{currentPlayer}</h2> */}
                 <div className="container-points">
                     <div className="form-control">
                         <input id="points-hand" type="number" autoFocus required />
                         <label><span>Hand Points</span></label>
                     </div>
 
-                    {
-                        cribHand && (
-                            <div id="crib" className="form-control">
-                                <input id="points-crib" type="number" required />
-                                <label><span>Crib Points</span></label>
-                            </div>
-                        )
-                    }
+                    <div id="crib" className="form-control">
+                        <input id="points-crib" type="number" required />
+                        <label><span>Crib Points</span></label>
+                    </div>
                 </div>
 
-                <button className="btn btn-info mt-2" onClick={() => nextTurn({ name: "Me", order: 1, currentScore: 3})}>
+                <button className="btn btn-info mt-2" onClick={() => nextTurn({ name: currentPlayer?.name, order: currentPlayer?.order, currentScore: 2})}>
                     Next Turn <i className="fa-solid fa-circle-chevron-right"></i>
                 </button>
 
